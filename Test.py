dd4093a4f5496e7f5b17fd139b0387cd21286785
George Whittle <gwhittle110@gmail.com>
2023-10-28 10:32:33 +0100

    Confirmed Claude API support is working

--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
 """ Testing for gitHub integration """
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+from dotenv import dotenv_values
+
+anthropic = Anthropic(
+    api_key=dotenv_values()['ANTHROPIC_API_KEY']
+)
+
+
+completion = anthropic.completions.create(
+    model="claude-2",
+    max_tokens_to_sample=300,
+    prompt=f"{HUMAN_PROMPT} how does a court case get to the Supreme Court?{AI_PROMPT}",
+)
+print(completion.completion)
